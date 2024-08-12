d6ce226ff37b36aac43ac7af368f5cf9ff43ac9e
kindmemento <kindmemento@gmail.com>
2024-08-12 17:26:41 +0300

    rename file

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+const admin = require('firebase-admin')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+admin.initializeApp({
+	credential: admin.credential.cert({
+		projectId: process.env.FIREBASE_PROJECT_ID,
+		privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+		clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+	}),
+	databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
+})
+
+const db = admin.firestore()
+
+module.exports = db
