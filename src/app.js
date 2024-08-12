b5b46ee60ec3775db17ee91ad8c28abf62b3e76e
kindmemento <kindmemento@gmail.com>
2024-08-12 17:42:44 +0300

    set up app to use parameter route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
-
 const app = express()
+const parmaeterRoutes = require('./route/parameterRoutes')
 
 app.use(express.json())
+app.use('/parameters', parmaeterRoutes)
 
 module.exports = app
