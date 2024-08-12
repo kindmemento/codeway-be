fd7e6b2a8485bb68361bdc2bdf80d2fec92a083f
kindmemento <kindmemento@gmail.com>
2024-08-12 21:51:35 +0300

    rename file

--- src/middleware/apiTokenMiddleware.js
+++
@@ -1,9 +0,0 @@
-const apiTokenMiddleware = (req, res, next) => {
-	const apiToken = req.headers['cc-api-token']
-	if (apiToken !== process.env.API_TOKEN) {
-		return res.status(403).json({ message: 'Forbidden' })
-	}
-	next()
-}
-
-module.exports = apiTokenMiddleware
