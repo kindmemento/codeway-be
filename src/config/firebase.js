5a7592a1c2e7e7e9e1924a03516ae517ca4dc26c
kindmemento <kindmemento@gmail.com>
2024-08-12 20:10:28 +0300

    temporarily use json file for initialization

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,16 +1,22 @@
 const admin = require('firebase-admin')
 const dotenv = require('dotenv')
-
 dotenv.config()
+const serviceAccount = require('../../firebaseServiceAccountKey.json')
 
 admin.initializeApp({
-	credential: admin.credential.cert({
-		projectId: process.env.FIREBASE_PROJECT_ID,
-		privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-		clientEmail: process.env.FIREBASE_CLIENT_EMAIL
-	}),
-	databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
+	credential: admin.credential.cert(serviceAccount),
+	databaseUrl: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
 })
+
+// @TODO: Fix the issue with env variables
+// admin.initializeApp({
+// 	credential: admin.credential.cert({
+// 		projectId: process.env.FIREBASE_PROJECT_ID,
+// 		privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n"),
+// 		clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+// 	}),
+// 	databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
+// })
 
 const db = admin.firestore()
 
