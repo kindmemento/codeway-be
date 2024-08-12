41c989a32c43607dda2d5def0009f62141452848
kindmemento <kindmemento@gmail.com>
2024-08-13 00:52:13 +0300

    add mobile routes to express app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const panelRoutes = require('./routes/panelRoutes')
+const mobileRoutes = require('./routes/mobileRoutes')
 
 app.use(express.json())
 app.use('/api', panelRoutes)
+app.use('/api', mobileRoutes)
 
 module.exports = app
