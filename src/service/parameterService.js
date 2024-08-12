e3317099ca8ca4d2b05bcf1bb77df4a2af8618b8
kindmemento <kindmemento@gmail.com>
2024-08-12 17:35:08 +0300

    implement parameter service with getAll and update service functions

--- src/service/parameterService.js
+++ src/service/parameterService.js
@@ -1,10 +1,20 @@
-const db = require('../config/firestore')
+const { db }= require('../config/firebase')
 
-const getParameters = async () => {
+const getAllParameters = async () => {
+	const parameters = []
 	const snapshot = await db.collection('parameters').get()
-	return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+	snapshot.forEach(doc => {
+		parameters.push({ id: doc.id, ...doc.data() })
+	})
+	return parameters
+}
+
+// @TODO: Test this out
+const updateParameters = async (id, data) => {
+	await db.collection('parameters').doc(id).set(data, { merge: true })
 }
 
 module.exports = {
-	getParameters
+	getAllParameters,
+	updateParameters
 }
