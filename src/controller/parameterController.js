0ec62a4bf7ed8ffa9f9ca7e89493cfbb72a3181a
kindmemento <kindmemento@gmail.com>
2024-08-12 17:39:16 +0300

    implement parameterController

---
+++ src/controller/parameterController.js
@@ -0,0 +1,27 @@
+const parameterService = require('../service/parameterService')
+
+const getParameters = async (req, res) => {
+	try {
+		const parameters = await parameterService.getAllParameters()
+		res.json(parameters)
+	} catch (error) {
+		res.status(500).json({ message: 'Failed to retrieve parameters' })
+	}
+}
+
+const updateParameter = async (req, res) => {
+	const { id } = req.params
+	const data = req.body
+
+	try {
+		await parameterService.updateParameter(id, data)
+		res.json({ message: 'Parameter updated successfully' })
+	} catch (error) {
+		res.status(500).json({ message: 'Failed to update parameter' })
+	}
+}
+
+module.exports = {
+	getParameters,
+	updateParameter
+}
