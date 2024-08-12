f3aef2e2ad6472b0861160464503e9f3dfeb7084
kindmemento <kindmemento@gmail.com>
2024-08-12 17:29:02 +0300

    import firebase admin from config, refactor return statements

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,15 +1,15 @@
-const admin = require('firebase-admin')
+const { admin } = require('../config/firebase')
 
 const authMiddleware = async (req, res, next) => {
-	const idToken = req.headers.authorization
-	if (!idToken) return res.status(401).send('Unauthorized')
+	const idToken = req.headers.authorization?.split(' ')[1]
+	if (!idToken) return res.status(401).json({ message: 'Unauthorized' })
 
 	try {
 		const decodedToken = await admin.auth().verifyIdToken(idToken)
 		req.user = decodedToken
 		next()
 	} catch (error) {
-		res.status(401).send('Invalid token')
+		res.status(401).json({ message: 'Invalid or expired token' })
 	}
 }
 
