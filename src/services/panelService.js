3daf52a12bd29a34e30f0f305e8d1dcaaff778c8
kindmemento <kindmemento@gmail.com>
2024-08-13 19:42:16 +0300

    rewrite get and create param service functions

--- src/services/panelService.js
+++ src/services/panelService.js
@@ -1,29 +1,48 @@
-const ParameterModel = require('../models/parameterModel')
+const admin = require('../config/firebase')
+const firestore = admin.firestore()
 
 const createParameter = async (data) => {
-	// @TODO: Validate incoming data before sending a request
-	const newParam = await ParameterModel.add(data)
-	return { id: newParam.id, ...data }
+	try {
+		const now = admin.firestore.Timestamp.now()
+
+		const dataWithTimestamp = {
+			...data,
+			created: now
+		}
+
+		const newParamRef = await firestore.collection('parameters').add(dataWithTimestamp)
+
+		const newParamDoc = await newParamRef.get()
+		const newParamData = newParamDoc.data()
+
+		return {
+			id: newParamRef.id,
+			...newParamData
+		}
+	} catch (error) {
+		console.error('Error creating parameter:', error)
+		throw error
+	}
 }
 
 const getAllParameters = async () => {
-	const snapshot = await ParameterModel.get()
+	const snapshot = await firestore.collection('parameters').get()
 	const params = []
 	snapshot.forEach(doc => params.push({ id: doc.id, ...doc.data() }))
 	return params
 }
 
-const updateParameter = async (id, data) => {
-	// @TODO: Validate incoming data before sending a request
-	// @TODO: In case of simultaneous requests, process the first one, reject the rest
-	await ParameterModel.doc(id).update(data)
-	return { id, ...data }
-}
+// const updateParameter = async (id, data) => {
+// 	// @TODO: Validate incoming data before sending a request
+// 	// @TODO: In case of simultaneous requests, process the first one, reject the rest
+// 	await ParameterModel.doc(id).update(data)
+// 	return { id, ...data }
+// }
 
-const deleteParameter = async (id) => {
-	await ParameterModel.doc(id).delete()
-	return { id }
-}
+// const deleteParameter = async (id) => {
+// 	await ParameterModel.doc(id).delete()
+// 	return { id }
+// }
 
 module.exports = {
 	createParameter,
