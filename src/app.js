607d9bc3f4cfce78cbd93cd0f8fe4e97e262e7d2
kindmemento <kindmemento@gmail.com>
2024-08-13 18:43:18 +0300

    set app to use cors options

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const panelRoutes = require('./routes/panelRoutes')
 const mobileRoutes = require('./routes/mobileRoutes')
 
+const FRONTEND_URL = process.env.FRONTEND_PANEL_URL
+
+const corsOptions = {
+	origin: FRONTEND_URL,
+	methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+	allowedHeaders: 'Content-Type, Authorization',
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use('/api', panelRoutes)
 app.use('/api', mobileRoutes)
