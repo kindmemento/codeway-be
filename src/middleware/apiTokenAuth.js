5b312c31f7f89aab7f78fde370b54006018a6cb5
kindmemento <kindmemento@gmail.com>
2024-08-13 18:36:38 +0300

    parse authHeader from authorization header

--- src/middleware/apiTokenAuth.js
+++ src/middleware/apiTokenAuth.js
@@ -1,7 +1,7 @@
 const API_TOKEN = process.env.API_TOKEN
 
 const apiTokenAuth = (req, res, next) => {
-	const authHeader = req.headers['cc-api-token']
+	const authHeader = req.headers['authorization'] ? req.headers['authorization'].split(' ')[1] : null
 
 	if (authHeader !== API_TOKEN) {
 		return res.status(403).json({ message: 'Unauthorized: Invalid or missing API token' })
