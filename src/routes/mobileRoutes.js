50969adb89c131d6a38d762da0e489730615b743
kindmemento <kindmemento@gmail.com>
2024-08-14 14:45:06 +0300

    add apiTokenAuth middleware to mobile route

--- src/routes/mobileRoutes.js
+++ src/routes/mobileRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const { getMobileConfiguration } = require('../controllers/mobileContoller')
+const apiTokenAuth = require('../middleware/apiTokenAuth')
 
 const router = express.Router()
 
-router.get('/mobile/config', getMobileConfiguration)
+router.get('/mobile/config', apiTokenAuth, getMobileConfiguration)
 
 module.exports = router
